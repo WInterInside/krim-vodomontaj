021292a43632aa2b5aea33e7fe9dbb5e88df6190
WinterInside <de_animator@mail.ru>
2021-11-24 02:41:07 +0300

    add accordion and price table

--- js/service.js
+++ js/service.js
@@ -24,3 +24,50 @@ $(document).ready(function(){
 		]
 	});
 });
+
+var price__accordion = (function (element) {
+	var _getItem = function (elements, className) { 
+		var element = undefined;
+		elements.forEach(function (item) {
+		if (item.classList.contains(className)) {
+			element = item;
+		}
+	});
+	return element;
+	};
+	return function () {
+	var _mainElement = {},
+	_items = {}, 
+	_contents = {}; 
+	var _actionClick = function (e) {
+	if (!e.target.classList.contains('price__accordion-item-header')) {
+		return;
+	}
+	e.preventDefault(); 
+	var header = e.target,
+	item = header.parentElement,
+	itemActive = _getItem(_items, 'show');
+	if (itemActive === undefined) { 
+		item.classList.add('show');
+	} else {
+		itemActive.classList.remove('show');
+		if (itemActive !== item) {			
+			item.classList.add('show');
+		}
+	}
+	},
+	_setupListeners = function () {
+		_mainElement.addEventListener('click', _actionClick);
+	};
+	return {
+		init: function (element) {
+			_mainElement = (typeof element === 'string' ? document.querySelector(element) : element);
+			_items = _mainElement.querySelectorAll('.price__accordion-item');
+			_setupListeners();
+		}
+	}
+	}
+})();
+
+var price__accordion1 = price__accordion();
+price__accordion1.init('#price__accordion');
